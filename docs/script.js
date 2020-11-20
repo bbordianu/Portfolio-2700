667181e9411bb4a405514926eb940f6a4a8c7431
bbordianu <b.bordianu@yahoo.com>
2020-11-19 23:06:01 -0500

    changes

--- docs/script.js
+++ docs/script.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function () {
   function success() {
     form.reset();
     status.classList.add("success");
-    status.innerHTML = "Thanks!";
+    status.innerHTML = "Thank you! I'll be in touch.";
   }
 
   function error() {
